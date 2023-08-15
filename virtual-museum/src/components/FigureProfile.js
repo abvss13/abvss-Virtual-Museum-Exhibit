88987ae7f14103831c92bdd61a672255bee4ae9c
Your Name <abdullahi.abass@student.moringschool.com>
2023-08-15 14:07:13 +0300

    20

--- virtual-museum/src/components/FigureProfile.js
+++ virtual-museum/src/components/FigureProfile.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
+
 const FigureProfile = ({ figure }) => (
   <div className="figure-profile">
-    <h3 className="figure-profile-name">{figure.name}</h3>
-    <img className="figure-profile-image" src={figure.img_url} alt={figure.name} />
-    <p className="figure-profile-description">{figure.description}</p>
+    <h3 className="figure-name">{figure.name}</h3>
+    <img className="figure-image" src={figure.img_url} alt={figure.name} />
+    <p className="figure-description">{figure.description}</p>
     {/* Display accomplishments or other details here */}
+    <div className="figure-accomplishments">
+      <h4>Accomplishments:</h4>
+      <ul>
+        {/* Map over figure's accomplishments and display them */}
+        {figure.accomplishments.map((accomplishment, index) => (
+          <li key={index}>{accomplishment}</li>
+        ))}
+      </ul>
+    </div>
   </div>
 );
 
