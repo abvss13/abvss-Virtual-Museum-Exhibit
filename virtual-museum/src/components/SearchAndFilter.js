98025575ee7e29fe050c22f7bb37628ae3ee9264
Your Name <abdullahi.abass@student.moringschool.com>
2023-08-15 13:49:49 +0300

    6

---
+++ virtual-museum/src/components/SearchAndFilter.js
@@ -0,0 +1,10 @@
+// SearchAndFilter.js
+import React from 'react';
+
+const SearchAndFilter = () => (
+  <div className="search-and-filter">
+    {/* Add your search and filtering components here */}
+  </div>
+);
+
+export default SearchAndFilter;
