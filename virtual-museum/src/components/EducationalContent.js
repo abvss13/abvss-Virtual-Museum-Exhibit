57cc1e7f5d7ef942400b2d91ad34f133e02f285f
Your Name <abdullahi.abass@student.moringschool.com>
2023-08-15 13:51:14 +0300

    7

---
+++ virtual-museum/src/components/EducationalContent.js
@@ -0,0 +1,10 @@
+// EducationalContent.js
+import React from 'react';
+
+const EducationalContent = () => (
+  <div className="educational-content">
+    {/* Add your educational content components here */}
+  </div>
+);
+
+export default EducationalContent;
