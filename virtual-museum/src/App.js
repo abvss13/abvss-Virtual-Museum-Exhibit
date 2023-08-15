cf6899ea3c841928f87f9c6ce9d5d6871547f742
Your Name <abdullahi.abass@student.moringschool.com>
2023-08-15 13:46:44 +0300

    1

--- virtual-museum/src/App.js
+++ virtual-museum/src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import ExhibitRoom from './components/ExhibitRoom';
+import FigureProfile from './components/FigureProfile';
+import fetchData from './FetchData';
+import './App.css'; // Import your CSS file here if needed
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData().then((fetchedData) => {
+      setData(fetchedData);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Virtual Museum Exhibit</h1>
+        {data.map((figure, index) => (
+          <ExhibitRoom key={index} roomName={`Exhibit Room ${index + 1}`}>
+            <FigureProfile figure={figure} />
+            {/* Include other components such as InteractiveElements, ArtifactDisplay, etc. */}
+          </ExhibitRoom>
+        ))}
       </header>
     </div>
   );
