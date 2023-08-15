de9071bd76e90e653eb2b650ab4e29da7117a240
Your Name <abdullahi.abass@student.moringschool.com>
2023-08-15 14:07:47 +0300

    21

--- virtual-museum/src/components/InteractiveElements.js
+++ virtual-museum/src/components/InteractiveElements.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+
 const InteractiveElements = () => (
   <div className="interactive-elements">
     {/* Add your interactive elements here */}
+    {/* For example: buttons, hotspots, etc. */}
   </div>
 );
 
