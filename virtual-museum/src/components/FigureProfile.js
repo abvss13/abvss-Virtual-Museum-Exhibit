9eb89b3a31202e3b41df767cff65d7def2d5de83
Your Name <abdullahi.abass@student.moringschool.com>
2023-08-15 13:58:47 +0300

    12

--- virtual-museum/src/components/FigureProfile.js
+++ virtual-museum/src/components/FigureProfile.js
@@ -1,11 +1,10 @@
-// FigureProfile.js
 import React from 'react';
 
 const FigureProfile = ({ figure }) => (
   <div className="figure-profile">
-    <h3>{figure.name}</h3>
-    <img src={figure.img_url} alt={figure.name} />
-    <p>{figure.description}</p>
+    <h3 className="figure-profile-name">{figure.name}</h3>
+    <img className="figure-profile-image" src={figure.img_url} alt={figure.name} />
+    <p className="figure-profile-description">{figure.description}</p>
     {/* Display accomplishments or other details here */}
   </div>
 );
