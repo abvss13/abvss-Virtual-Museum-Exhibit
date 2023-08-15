974c9038662bf21bfa00d4cb818ca7f385734a96
Your Name <abdullahi.abass@student.moringschool.com>
2023-08-15 13:49:22 +0300

    5

---
+++ virtual-museum/src/components/ArtifactDisplay.js
@@ -0,0 +1,10 @@
+// ArtifactDisplay.js
+import React from 'react';
+
+const ArtifactDisplay = () => (
+  <div className="artifact-display">
+    {/* Add your artifact display components here */}
+  </div>
+);
+
+export default ArtifactDisplay;
