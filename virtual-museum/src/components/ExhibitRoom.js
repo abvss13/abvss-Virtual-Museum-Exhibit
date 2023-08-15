c435e7b3699a923a600beec65e95728b46ceeba1
Your Name <abdullahi.abass@student.moringschool.com>
2023-08-15 14:06:35 +0300

    19

--- virtual-museum/src/components/ExhibitRoom.js
+++ virtual-museum/src/components/ExhibitRoom.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
+
 const ExhibitRoom = ({ roomName, children }) => (
   <div className="exhibit-room">
-    <h2>{roomName}</h2>
-    <div className="exhibit-room-content">{children}</div>
+    <h2 className="room-name">{roomName}</h2>
+    <div className="room-content">{children}</div>
   </div>
 );
 
