e06f5d37cbf6dff4df83017ae4090827c79815da
Your Name <abdullahi.abass@student.moringschool.com>
2023-08-15 18:19:24 +0300

    25

--- virtual-museum/src/App.js
+++ virtual-museum/src/App.js
@@ -1,40 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import ExhibitRoom from './components/ExhibitRoom';
-import FigureProfile from './components/FigureProfile';
-import InteractiveElements from './components/InteractiveElements'; // Import InteractiveElements
-import ArtifactDisplay from './components/ArtifactDisplay'; // Import ArtifactDisplay
-import SearchAndFilter from './components/SearchAndFilter'; // Import SearchAndFilter
-import EducationalContent from './components/EducationalContent'; // Import EducationalContent
-import Quiz from './components/Quiz'; // Import Quiz
-import fetchData from './FetchData';
-import './App.css'; // Import your CSS file here if needed
+import React from 'react';
+import HistoricalFigureList from './HistoricalFigureList'; // Correct relative path
+import './App.css'
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetchData().then((fetchedData) => {
-      if (fetchedData && fetchedData.historic_figures) {
-        setData(fetchedData.historic_figures);
-      }
-    });
-  }, []);
-
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Virtual Museum Exhibit</h1>
-        {data.map((figure, index) => (
-          <ExhibitRoom key={index} roomName={`Exhibit Room ${index + 1}`}>
-            <FigureProfile figure={figure} />
-            <InteractiveElements /> {/* Add InteractiveElements component */}
-            <ArtifactDisplay /> {/* Add ArtifactDisplay component */}
-            <SearchAndFilter /> {/* Add SearchAndFilter component */}
-            <EducationalContent /> {/* Add EducationalContent component */}
-            <Quiz /> {/* Add Quiz component */}
-          </ExhibitRoom>
-        ))}
+        <h1>Somali Historical Figures Encyclopedia</h1>
       </header>
+      <main>
+        <HistoricalFigureList />
+      </main>
     </div>
   );
 }
---
+++ virtual-museum/src/HistoricalFigureList.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import './App.css'; // Import your CSS file
+
+function HistoricalFigureList() {
+  const [historicFigures, setHistoricFigures] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [expandedFigure, setExpandedFigure] = useState(null);
+
+  useEffect(() => {
+    // Fetch data from the API
+    fetch('https://api.npoint.io/f3fd7456f67459fc2386') // Provided API endpoint
+      .then(response => response.json())
+      .then(data => {
+        setHistoricFigures(data.historic_figures);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        setError(error);
+        setIsLoading(false);
+      });
+  }, []);
+
+  const toggleExpansion = index => {
+    if (expandedFigure === index) {
+      setExpandedFigure(null);
+    } else {
+      setExpandedFigure(index);
+    }
+  };
+
+  const handleClose = () => {
+    setExpandedFigure(null);
+  };
+
+  if (isLoading) {
+    return <div className="loading">Loading...</div>;
+  }
+
+  if (error) {
+    return <div className="error">Error: {error.message}</div>;
+  }
+
+  return (
+    <div className="figure-list">
+      {historicFigures.map((figure, index) => (
+        <div key={index} className={`figure ${expandedFigure === index ? 'expanded' : ''}`}>
+          <img src={figure.img_url} alt={figure.name} />
+          <h2>{figure.name}</h2>
+          {expandedFigure === index ? (
+            <div className="expanded-info">
+              <p>{figure.description}</p>
+              <div className="accomplishments">
+                <h3>Accomplishments:</h3>
+                <ul>
+                  {figure.accomplishments.map((accomplishment, idx) => (
+                    <li key={idx}>{accomplishment}</li>
+                  ))}
+                </ul>
+              </div>
+              <button className="close-button" onClick={handleClose}>Close</button>
+            </div>
+          ) : (
+            <button className="more-info-button" onClick={() => toggleExpansion(index)}>More Info</button>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default HistoricalFigureList;
