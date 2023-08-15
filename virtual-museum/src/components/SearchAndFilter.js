807f103518199038f306430a27127567ed83b512
Your Name <abdullahi.abass@student.moringschool.com>
2023-08-15 14:09:11 +0300

    23

--- virtual-museum/src/components/SearchAndFilter.js
+++ virtual-museum/src/components/SearchAndFilter.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const SearchAndFilter = () => (
-  <div className="search-and-filter">
-    {/* Add your search and filtering components here */}
-  </div>
-);
+
+const SearchAndFilter = ({ figures, onFilter }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+    onFilter(event.target.value);
+  };
+
+  return (
+    <div className="search-and-filter">
+      <input
+        type="text"
+        placeholder="Search by name"
+        value={searchTerm}
+        onChange={handleSearch}
+        className="search-input"
+      />
+    </div>
+  );
+};
 
 export default SearchAndFilter;
